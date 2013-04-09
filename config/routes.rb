c26dde5863759e980aee717b387717aff022af77
saasbook <saasbook@saasbook.(none)>
2013-04-09 02:57:00 -0700

    change of routing for "/index"
     Committer: Elitza Maneva <elitza.maneva@gmail.com>
    
     On branch master
     Changes to be committed:
       (use "git reset HEAD <file>..." to unstage)
    
    	modified:   config/routes.rb
    	renamed:    public/index.html -> public/index_old.html

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@ Rottenpotatoes::Application.routes.draw do
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
+  # You can have the root of your site routed with "root"
+  # just remember to delete public/index.html.
+  # root :to => 'welcome#index'
+
+  root :to => 'movies#index'
+
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
@@ -47,9 +53,6 @@ Rottenpotatoes::Application.routes.draw do
   #     resources :products
   #   end
 
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
 
